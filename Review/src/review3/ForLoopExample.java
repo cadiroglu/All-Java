0e65e5ab419d65f0d6b524146ae48b1224b665f4
Ridvan <cadiroglu34@gmail.com>
2023-01-27 16:09:40 -0600

    Review

---
+++ Review/src/review3/ForLoopExample.java
@@ -0,0 +1,30 @@
+package review3;
+
+public class ForLoopExample {
+    public static void main(String[] args) {
+
+        for (int i=10; i>5; i--){
+
+            System.out.println(i);
+        }
+
+        for(int i=5; i<=1; i--){
+            System.out.println("Happy");
+        }
+
+
+        for(int i=1; i<=6; i++){
+
+
+
+
+            if(i%2==0) {
+                System.out.println(" even number "+i);
+            }
+        }
+
+
+
+    }
+}
+
