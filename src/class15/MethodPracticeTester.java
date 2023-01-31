c22cf2a4baa6ac259e9e68bc8441e2537fa0c588
Ridvan <cadiroglu34@gmail.com>
2023-01-31 15:00:44 -0600

    New Classes

---
+++ src/class15/MethodPracticeTester.java
@@ -0,0 +1,17 @@
+package class15;
+
+public class MethodPracticeTester {
+    public static void main(String[] args) {
+
+        MethodPractice mp=new MethodPractice();
+        boolean isEven=   mp.isEven(15);
+        System.out.println(isEven);
+        // Break till 12:05
+        // call iseven method for the numbers 100 12 20
+        boolean isEven2=mp.isEven(100);
+        System.out.println(isEven2);
+        System.out.println(mp.isEven(12));
+        System.out.println(mp.isEven(20));
+
+    }
+}
---
+++ src/class16/PersonTester.java
@@ -0,0 +1,17 @@
+package class16;
+import java.util.Scanner;
+public class PersonTester {
+
+    public static void main(String[] args) {
+        Person person = new Person();
+        //  System.out.println(person.bankBalance);
+        //  System.out.println(person.address);
+        System.out.println(person.name);
+        person.printTikTokAccount();
+        Scanner scanner = new Scanner(System.in);
+        scanner.next();
+
+
+    }
+
+    }
